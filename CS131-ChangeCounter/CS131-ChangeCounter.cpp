ef1847bffa14f022a8a5b426f87e4a15c1f2e46d
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-07 16:49:27 -0700

    Commented in assignment instructions.

--- CS131-ChangeCounter/CS131-ChangeCounter.cpp
+++ CS131-ChangeCounter/CS131-ChangeCounter.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/*
+Write a program that will calculate the value of some coins.
+
+Your program will need to ask the user how many 
+quarters (25 cent coin), dimes (10 cent coin), nickels (5 cent coin), and pennies (1 cent coin) 
+they have and then it will repeat back to them the quantity of each coin type and the total value 
+for all coins shown in typical dollar and cents format ($5.34).
+*/
+
 int main()
 {
     return 0;
